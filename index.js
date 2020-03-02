20675a60ff484245a27adfab95c88df40fc165c3
oscarali1985 <oscarali1985@hotmail.com>
2020-03-02 13:41:58 -0400

    finalizado

---
+++ index.js
@@ -0,0 +1,13 @@
+// https://www.nodebeginner.org/index-es.html
+
+
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.iniciar;
+handle["/iniciar"] = requestHandlers.iniciar;
+handle["/subir"] = requestHandlers.subir;
+
+server.iniciar(router.route, handle);
